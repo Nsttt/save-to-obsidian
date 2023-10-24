12c326669b3521e51f9b557638d1d809e141fefc
Nestor <nestor@nstlopez.com>
2023-10-24 22:16:55 +0200

    Few tweaks to make code more readable

--- src/content.ts
+++ src/content.ts
@@ -1,9 +1,49 @@
 import { Readability } from "@mozilla/readability";
 import TurndownService from "turndown";
 
+function getFileName(title: string) {
+  const isWindows = window.navigator.userAgent.toLowerCase().includes("win");
+
+  const fileName = isWindows
+    ? title.replace(":", "").replace(/[/\\?%*|"<>]/g, "-")
+    : title.replace(":", "").replace(/\//g, "-").replace(/\\/g, "-");
+
+  return fileName;
+}
+
+function getSelectionHtml() {
+  let html = "";
+  const sel = window.getSelection();
+  if (sel && sel.rangeCount) {
+    let container = document.createElement("div");
+    for (let i = 0, len = sel.rangeCount; i < len; ++i) {
+      container.appendChild(sel.getRangeAt(i).cloneContents());
+    }
+    html = container.innerHTML;
+  }
+  return html;
+}
+
+function convertDate(date: Date) {
+  const yyyy = date.getFullYear().toString();
+  const mm = (date.getMonth() + 1).toString();
+  const dd = date.getDate().toString();
+  const mmChars = mm.split("");
+  const ddChars = dd.split("");
+
+  return (
+    yyyy +
+    "-" +
+    (mmChars[1] ? mm : "0" + mmChars[0]) +
+    "-" +
+    (ddChars[1] ? dd : "0" + ddChars[0])
+  );
+}
+
 function createNote() {
   /* Optional vault name */
   const vault = "";
+  const vaultName = vault ? "&vault=" + encodeURIComponent(`${vault}`) : "";
 
   /* Optional folder name such as "Clippings/" */
   const folder = "Encounters/";
@@ -11,66 +51,34 @@ function createNote() {
   /* Optional tags  */
   let tags = "clippings";
 
+  const selection = getSelectionHtml();
+  const today = convertDate(new Date());
+
+  const clonedDoc = document.cloneNode(true) as Document; // ? Have to do this, structuredClone doesn't work on DOM elements.
+  const readable = new Readability(clonedDoc).parse();
+
   /* parse and lightly clean the site's meta keywords content into tags, if present */
   const metaElement = document.querySelector<HTMLMetaElement>(
     'meta[name="keywords" i]'
   );
+
   if (metaElement) {
     const content = metaElement.getAttribute("content");
     if (content) {
       const keywords = content.split(",");
-      keywords.forEach(function (keyword) {
-        let tag = " " + keyword.split(" ").join("");
+      keywords.forEach((keyword) => {
+        const tag = " " + keyword.split(" ").join("");
         tags += tag;
       });
     }
   }
-
-  function getSelectionHtml() {
-    let html = "";
-    const sel = window.getSelection();
-    if (sel && sel.rangeCount) {
-      let container = document.createElement("div");
-      for (let i = 0, len = sel.rangeCount; i < len; ++i) {
-        container.appendChild(sel.getRangeAt(i).cloneContents());
-      }
-      html = container.innerHTML;
-    }
-    return html;
-  }
-
-  const selection = getSelectionHtml();
-
-  const clonedDoc = document.cloneNode(true) as Document;
-  const readable = new Readability(clonedDoc).parse();
 
   if (!readable) {
     console.error("Failed to parse the document with Readability.");
     return;
   }
 
-  const { title, byline, content } = readable;
-
-  function getFileName(fileName: string) {
-    const userAgent = window.navigator.userAgent.toLowerCase();
-
-    // Check if the user agent contains 'win', which indicates Windows platform
-    const isWindows = userAgent.includes("win");
-
-    if (isWindows) {
-      fileName = fileName.replace(":", "").replace(/[/\\?%*|"<>]/g, "-");
-    } else {
-      fileName = fileName
-        .replace(":", "")
-        .replace(/\//g, "-")
-        .replace(/\\/g, "-");
-    }
-    return fileName;
-  }
-
-  const fileName = getFileName(title);
-  const markdownify = selection ? selection : content;
-  const vaultName = vault ? "&vault=" + encodeURIComponent(`${vault}`) : "";
+  const fileName = getFileName(readable.title);
 
   const turndownService = new TurndownService({
     headingStyle: "atx",
@@ -79,35 +87,18 @@ function createNote() {
     codeBlockStyle: "fenced",
     emDelimiter: "*",
   });
+
+  const markdownify = selection ? selection : readable.content;
   const markdownBody = turndownService.turndown(markdownify);
-
-  let date = new Date();
-
-  function convertDate(date: Date) {
-    let yyyy = date.getFullYear().toString();
-    let mm = (date.getMonth() + 1).toString();
-    let dd = date.getDate().toString();
-    let mmChars = mm.split("");
-    let ddChars = dd.split("");
-    return (
-      yyyy +
-      "-" +
-      (mmChars[1] ? mm : "0" + mmChars[0]) +
-      "-" +
-      (ddChars[1] ? dd : "0" + ddChars[0])
-    );
-  }
-
-  const today = convertDate(date);
 
   /* YAML front matter as tags render cleaner with special chars  */
   const fileContent =
     "---\n" +
     "author:    " +
-    byline +
+    readable.byline +
     "\n" +
     "title:     [" +
-    title +
+    readable.title +
     "]\n" +
     "source:    " +
     document.URL +
