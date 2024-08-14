250f66ff7450e0fd0b411ee35b20af2fcab9236f
Néstor <nestor@nstlopez.com>
2024-08-14 16:00:30 +0200

    feat: create the context menu always intead than on install

--- src/background.ts
+++ src/background.ts
@@ -1,10 +1,9 @@
 import browser from "webextension-polyfill";
+
 // This will create a context menu item
-browser.runtime.onInstalled.addListener(() => {
-  browser.contextMenus.create({
-    id: "saveText",
-    title: "Save to Obsidian",
-  });
+browser.contextMenus.create({
+  id: "saveText",
+  title: "Save to Obsidian",
 });
 
 browser.contextMenus.onClicked.addListener((info, tab) => {
