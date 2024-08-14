b05d158a20484e8f3e11055af8bb90ac2525f4c3
Néstor <nestor@nstlopez.com>
2024-08-14 16:17:29 +0200

    feat: fix popup settings and add few nice features for defaults

--- src/popup.ts
+++ src/popup.ts
@@ -1,23 +1,47 @@
 import browser from "webextension-polyfill";
 
 const folderElement = document.getElementById(
-  "folder"
+	"folder",
 ) as HTMLInputElement | null;
 const tagsElement = document.getElementById("tags") as HTMLInputElement | null;
 const saveButtonElement = document.getElementById(
-  "saveButton"
+	"saveButton",
 ) as HTMLButtonElement | null;
 
-if (saveButtonElement) {
-  saveButtonElement.addEventListener("click", () => {
-    const defaultFolder = folderElement ? folderElement.value : "";
-    const defaultTags = tagsElement ? tagsElement.value : "";
+// Function to load the current values from sync storage
+const loadCurrentValues = async () => {
+	try {
+		const result = await browser.storage.sync.get([
+			"defaultFolder",
+			"defaultTags",
+		]);
 
-    browser.storage.sync.set({
-      defaultFolder: defaultFolder,
-      defaultTags: defaultTags,
-    });
-  });
+		if (folderElement) {
+			folderElement.value = result.defaultFolder || ""; // Set to empty string if not found
+		}
+
+		if (tagsElement) {
+			tagsElement.value = result.defaultTags || ""; // Set to empty string if not found
+		}
+	} catch (error) {
+		console.error("Failed to load current values:", error);
+	}
+};
+
+// Save button event listener
+if (saveButtonElement) {
+	saveButtonElement.addEventListener("click", () => {
+		const defaultFolder = folderElement ? folderElement.value.trim() : "";
+		const defaultTags = tagsElement ? tagsElement.value.trim() : "";
+
+		browser.storage.sync.set({
+			defaultFolder: defaultFolder,
+			defaultTags: defaultTags,
+		});
+	});
 } else {
-  console.error("Save button element not found");
+	console.error("Save button element not found");
 }
+
+// Load the current values when the script runs
+loadCurrentValues();
